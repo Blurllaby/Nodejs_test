522e2256f5c01ec5916dc269d3c1586ed8900726
Blurllaby <106916011+Blurllaby@users.noreply.github.com>
2023-11-06 11:07:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,56 +16,60 @@ app.use(bodyParser.json());
 app.get('/login', (req, res) => {
   res.render('login');
 });
-
 app.post('/loginSubmit', (req, res) => {
-  const { username, password } = req.body;
-  console.log('Received username:', username);
-
-  const sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    server: 'duongdb.database.windows.net',
-    options: {
-      encrypt: true, // for Azure
-      trustServerCertificate: false, // change to true for local dev / self-signed certs
-    },
-  };
-
-  sql.connect(sqlConfig, err => {
-    // ... error checks
-
-    // Query
-    new sql.Request().query(`
-        select * from Users where
-        userName in ('${username}')
-        `, (err, result) => {
-      // ... error checks
-      console.log(err)
-      if (err) {
-        res.render('login', { message: 'Login error please check your input data' });
-      }
-      else {
-        console.dir(result);
-        let pass = result['recordset'][0]['PasswordID'];
-        console.log(result['recordset'])
-        if (pass == password) {
-          console.log('value present')
-          res.render('login', { message: 'success' });
+    console.log('login hit');
+    console.log(req.body)
+​
+    let username = req.body.username;
+    let password = req.body.password;
+​
+    const sqlConfig = {
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
+        server: 'techlitekr.database.windows.net',
+        pool: {
+            max: 100,
+            min: 0,
+            idleTimeoutMillis: 300000
+        },
+        options: {
+            encrypt: true, // for azure
+            trustServerCertificate: false // change to true for local dev / self-signed certs
         }
-        else {
-          res.render('login', { message: 'Login error please check your password' });
-        }
-
-      }
+    }
+​
+    sql.connect(sqlConfig, err => {
+        // ... error checks
+​
+        // Query
+        new sql.Request().query(`
+            select * from Users where
+            userName in ('${username}')
+            `, (err, result) => {
+            // ... error checks
+            console.log(err)
+            if (err) {
+                res.render('login', { message: 'Login error please check your input data' });
+            }
+            else {
+                console.dir(result);
+                let pass = result['recordset'][0]['PasswordID'];
+                console.log(result['recordset'])
+                if (pass == password) {
+                    console.log('value present')
+                    res.render('login', {message: true});
+                }
+                else{
+                    res.render('login', { message: 'Login error please check your password' });
+                }
+​
+            }
+        })
+​
     })
-
-  })
-
-
-
-
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
